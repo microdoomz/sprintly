bff9aae81016368043bf34740b9e36fab583ae71
microdoomz <junaidmohammad232@gmail.com>
2026-05-27 20:16:50 +0530

    Fix UTF-16 encoding in use-navigation-store and Toaster import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 import { GlobalLoader } from "@/components/layout/global-loader";
 import "./globals.css";
 
